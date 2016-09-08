2e5c3615ed718229d9915e08d474605f64fb987f
happinesssam <sam.bellman@gmail.com>
2016-09-08 10:40:21 +0000

    1st version

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express')
 var app = express();
+var chrono = require('chrono-node')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var monthNames = [
+  "January", "February", "March",
+  "April", "May", "June", "July",
+  "August", "September", "October",
+  "November", "December"
+];
+
+app.get('/:dateString', function (req, res) {
+    var tryNum = Number(req.params.dateString);
+    var date;
+    if(!isNaN(tryNum) && tryNum>=0 && tryNum%1==0){
+        date = new Date(tryNum * 1000);
+    }else{
+        var chDate = chrono.parseDate(req.params.dateString); 
+        if(chDate)date = new Date(chDate);
+    }
+    if(date){
+        res.send({ "unix": date.getTime()/1000, "natural": monthNames[date.getMonth()] +" " + date.getDate() + ", " + date.getFullYear()});
+    }else{
+        res.send({ "unix": null, "natural": null });
+    }
 });
 
 app.listen(8080);
